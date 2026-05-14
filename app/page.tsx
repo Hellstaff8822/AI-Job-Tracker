17ad7ee5ef30808b9d11cef6c699edf7c94686a5
Hellstaff8822 <fgjd.gh50@gmail.com>
2026-05-14 14:12:54 +0300

    fix-board

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function JobTrackerPage() {
       <div className='fixed top-[-10%] left-[-10%] w-[40%] h-[40%] rounded-full bg-blue-600/10 blur-[120px] pointer-events-none' />
       <div className='fixed bottom-[-10%] right-[-10%] w-[40%] h-[40%] rounded-full bg-purple-600/10 blur-[120px] pointer-events-none' />
 
-      <div className='relative z-10 max-w-[1920px] mx-auto flex flex-col h-full px-4 md:px-0 pb-10 lg:pb-0'>
+      <div className='relative z-10 max-w-[1920px] mx-auto flex flex-col h-full px-4 md:px-0 pb-4 lg:pb-0'>
         <header className='flex justify-between items-end mb-12'>
           <div>
             <h1 className='text-4xl font-black tracking-tighter bg-gradient-to-r from-white to-slate-500 bg-clip-text text-transparent leading-none'>
